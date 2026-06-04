8d4d4aa5de2745c10f3ab22610af82a43571a185
Spectraa28 <sonuvermacollege@gmail.com>
2026-06-04 18:55:25 +0530

    fix: removed the hardcoded port in the api file and docker file

--- src/api.py
+++ src/api.py
@@ -198,4 +198,5 @@ def run_benchmark():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
